8041b46a16ba4da4de6f6a62b41de96488e1c6a7
Vincentho711 <vincentho711@gmail.com>
2020-12-26 14:36:33 +0000

    Added MACD and VWAP indicators

--- robot/indicator.py
+++ robot/indicator.py
@@ -131,16 +131,18 @@ class Indicators():
     def price_df(self,price_df:pd.DataFrame) -> None:
         self._frame = price_df
 
-    def change_in_price(self) -> pd.DataFrame:
-        """Calculates the Change in Price.
+    def change_in_price(self,column_name:str = 'change_in_price') -> pd.DataFrame:
+        """Calaculate the change in close price
+
+        Args:
+            column_name (str, optional): Pass in a value if you wish to change the column name. Defaults to 'change_in_price'.
+
         Returns:
-        ----
-        {pd.DataFrame} -- A data frame with the Change in Price included.
+            pd.DataFrame: Returns a pd dataframe with added column 'change_in_price'
         """
         locals_data = locals()      #Capture information passed through as arguments in a local symbol table, it changes depending where you can it
         del locals_data['self']     #delete the 'self' key as it doesn't matter, we only care about the arguments we pass through besides 'self'
 
-        column_name = 'change_in_price'            #Create an additional column in the dataframe
         self._current_indicators[column_name] = {}      #Create a new dictionary with key 'change_in_price' to be placed in our current indicators dictionary
         self._current_indicators[column_name]['args'] = locals_data      #Create a new dictionary with key 'args' to be placed in our _current_indicators[column_name] dict
                                                                          #The values are the arguments passed to the function, so it saves all our arguments passed to an object
@@ -153,11 +155,23 @@ class Indicators():
 
         return self._frame
 
-    def rsi(self,period:int,method:str='wilders') ->pd.DataFrame:
+    # RSI
+    def rsi(self,period:int,method:str='wilders',column_name:str = 'rsi') ->pd.DataFrame:
+        """RSI (Relative Strength Index) measures the magnitude of recent price changes to evaluate overbought or 
+        oversold conditions in the price of a stock or other asset. Traders may sell when RSI>0.7 and buy when RSI<0.3.
+
+        Args:
+            period (int): The period used to calculate the exponential moving average. A typical value would be 14.
+            method (str, optional): Method used to calculate rsi. Defaults to 'wilders'.
+            column_name (str, optional): Pass in a value if you wish to change the column name. Defaults to 'rsi'.
+
+        Returns:
+            pd.DataFrame: Returns a pd dataframe with added column 'rsi_period'
+        """
         locals_data = locals()
         del locals_data['self']
 
-        column_name = 'rsi'
+        column_name = column_name + '_' + str(period)
         self._current_indicators[column_name] = {}
         self._current_indicators[column_name]['args'] = locals_data
         self._current_indicators[column_name]['func'] = self.rsi
@@ -171,20 +185,20 @@ class Indicators():
         )
 
         self._frame['down_day'] = self._price_groups['change_in_price'].transform(
-            lambda x: np.where(x<=0,x.abs(),0)        #Return elements chosen from x or y depending on condition, if x<=0, x=x.abs(), elif x > 0, return 0, only keep negative values
+            lambda x: np.where(x<0,x.abs(),0)        #Return elements chosen from x or y depending on condition, if x<=0, x=x.abs(), elif x > 0, return 0, only keep negative values
         )
 
         self._frame['ewma_up'] = self._price_groups['up_day'].transform(
-            lambda x: x.ewm(span = period).mean()        #Give rolling average on up_day
+            lambda x: x.ewm(com = period-1).mean()        #Give rolling average on up_day
         )
 
         self._frame['ewma_down'] = self._price_groups['down_day'].transform(
-            lambda x: x.ewm(span = period).mean()        #Give rolling average on up_day
+            lambda x: x.ewm(com = period-1).mean()        #Give rolling average on up_day
         )
         relative_strength = self._frame['ewma_up']/self._frame['ewma_down']
         relative_strength_index = 100.0 - (100.0/ (1.0 + relative_strength))   #Using RSI formula
 
-        self._frame['rsi'] = np.where(relative_strength_index==0,100, 100 - (100 / (1 + relative_strength_index)))   #Step 2 of the RSI calculation
+        self._frame[column_name] = np.where(relative_strength_index==0,100, relative_strength_index)   # Deal with cases when rsi = 0
 
         # Clean up before sending back. Delete all the unnessary columns and just leave 'rsi' in place
         self._frame.drop(
@@ -195,12 +209,21 @@ class Indicators():
 
         return self._frame
 
-    #Simple moving average
-    def sma(self, period:int) -> pd.DataFrame:
+    # Simple moving average
+    def sma(self, period:int,column_name:str = 'sma') -> pd.DataFrame:
+        """SMA (Simple Moving Average) meausres the trend of price movement over a defined period.
+
+        Args:
+            period (int): The period used to calculate the sma. Typical values would be 5,10,20 and 50
+            column_name (str, optional): Pass in a value if you wish to change the column name. Defaults to 'sma'.
+
+        Returns:
+            pd.DataFrame: Returns a pd dataframe with added column 'sma_period'
+        """
         locals_data = locals()
         del locals_data['self']
 
-        column_name = 'sma'
+        column_name = column_name + '_' + str(period)
         self._current_indicators[column_name] = {}
         self._current_indicators[column_name]['args'] = locals_data
         self._current_indicators[column_name]['func'] = self.sma
@@ -210,12 +233,22 @@ class Indicators():
         )
 
         return self._frame
+    # Exponential Moving Average
+    def ema(self, period:int, alpha: float = 0.0,column_name:str = 'ema') -> pd.DataFrame:
+        """EMA (Exponential Moving Average)
 
-    def ema(self, period:int, alpha: float = 0.0) -> pd.DataFrame:
+        Args:
+            period (int): The period used to calculate ema. Typical value: 12/26 for short term, 50/200 for long term
+            alpha (float, optional): [description]. Defaults to 0.0.
+            column_name (str, optional): Pass in a value if you wish to change the column name. Defaults to 'ema'.
+
+        Returns:
+            pd.DataFrame: Returns a pd dataframe with added column 'ema_period'
+        """
         locals_data = locals()
         del locals_data['self']
 
-        column_name = 'ema'
+        column_name = column_name + '_' + period
         self._current_indicators[column_name] = {}
         self._current_indicators[column_name]['args'] = locals_data
         self._current_indicators[column_name]['func'] = self.ema
@@ -225,6 +258,73 @@ class Indicators():
         )
 
         return self._frame
+
+    # MACD
+    def macd(self,fast_period:int = 12,slow_period:int = 26,column_name:str = 'macd') -> pd.DataFrame:
+        """MACD(Moving Average Convergence Divergence) is a trend following momentum indicator that shows the
+        relationships between 2 moving averages, tpically ema. Traders may buy the security when 'macd' crosses
+        above the 'macd_signal' line and sell when 'macd' goes below the 'macd_signal' line.
+        
+
+        Args:
+            fast_period (int, optional): The period used to calculate the ema of a small window. Defaults to 12.
+            slow_period (int, optional): The period used to calculate the ema of a long window. Defaults to 26.
+            column_name (str, optional): The name of column. Defaults to 'macd'.
+
+        Returns:
+            pd.DataFrame: returns a pd Dataframe with added columns 'macd_fast','macd_slow','macd' and 'macd_signal'
+        """
+        locals_data = locals()
+        del locals_data['self']
+
+        self._current_indicators[column_name] = {}
+        self._current_indicators[column_name]['args'] = locals_data
+        self._current_indicators[column_name]['func'] = self.macd
+
+        # Calculate fast moving macd
+        self._frame['macd_fast'] = self._frame['close'].transform(
+            lambda x: x.ewm(span = fast_period, min_periods = fast_period).mean()
+        )
+
+        # Calculate slow moving macd
+        self._frame['macd_slow'] = self._frame['close'].transform(
+            lambda x: x.ewm(span = slow_period, min_periods = slow_period).mean()
+        )
+
+        # Calculate the difference between fast and slow macd
+        self._frame['macd'] = self._frame['macd_fast'] - self._frame['macd_slow']
+
+        # Calculate the exponential moving average of the macd_diff
+        self._frame['macd_signal'] = self._frame['macd'].transform(
+            lambda x: x.ewm(span=9,min_periods=8).mean()
+        )
+
+        return self._frame
+    
+    # VWAP
+    def vwap(self,column_name='vwap') -> pd.DataFrame:
+        """VWAP is the volumn weighted average price, typically used to calculate 
+        the average price a security has traded at throughout the day/minute. It 
+        provides insight into both the trend and value of a security.
+
+        Returns:
+            pd.DataFrame: Returns a pd Dataframe with added column 'vwap'
+        """
+        locals_data = locals()
+        del locals_data['self']
+
+        self._current_indicators[column_name] = {}
+        self._current_indicators[column_name]['args'] = locals_data
+        self._current_indicators[column_name]['func'] = self.vwap
+
+        high = self._frame['high']
+        low = self._frame['low']
+        close = self._frame['close']
+        volume = self._frame['volume']
+        
+        self._frame['vwap'] = (volume*(high+low+close)/3).cumsum() / volume.cumsum()
+        return self._frame
+    
 
     #refresh all the indicators every time a new row is added
     def refresh(self):
